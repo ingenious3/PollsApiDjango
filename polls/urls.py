13358f6b87b7f296e2b2f68f2705de14e2b8f8a2
ingenious3 <ishant.7123217@gmail.com>
2019-05-29 13:37:57 +0530

    basic api with DRF

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import polls_details, polls_list
+# from .views import polls_details, polls_list
+from .apiviews import PollDetails, PollList
 
 urlpatterns = [
-	path('polls/', polls_list, name = 'polls_list'),
-	path('polls/<int:pk>/', polls_details, name = 'polls_details'),
+	path('polls/', PollList.as_view(), name = 'polls_list'),
+	path('polls/<int:pk>/', PollDetails.as_view(), name = 'polls_details'),
 ]
